00a2e5f6dbef8aeb11991c7c5069c3005bdb703c
GabrielVidal16 <gabrielvtxs@gmail.com>
2024-12-17 14:09:53 -0300

    stage

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ app.config['SESSION_PERMANENT'] = False
 app.register_blueprint(usuarios_bp)
 app.register_blueprint(home_bp)
 app.register_blueprint(produtos_bp)
-<<<<<<< HEAD
-
-=======
->>>>>>> 79ce879a98c16ed441e62c46c001e172e33287cd
 
 @app.route('/')
 def index():
